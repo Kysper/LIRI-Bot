466d02456944a3e6b6fed03853051b0122da5dfd
Kysper <kysper50@gmail.com>
2018-10-20 00:31:30 -0500

    Product is ready for launch

--- liri.js
+++ liri.js
@@ -1,92 +1,161 @@
 // Adds the dependencies for the node packages
 require("dotenv").config();
-
-var  inquirer = require("inquirer");
 var fs = require("fs");
 var moment = require("moment")
- const keys = require('./keys.js');
- const Spotify = require('node-spotify-api');
- var request = require("request");
+const Spotify = require('node-spotify-api');
+const keys = require('./keys.js');
+var request = require("request");
 
 // Creates an object with keys for spotify
 const spotify = new Spotify(keys.spotify);
-console.log(spotify);
 
-// // Clears the node array
-// process.argv.splice(0,2);
+// Clears the node array
+process.argv.splice(0, 2);
 
-// //Variable that grabs the command argument
-// var commandArg = process.argv.shift();
-// var searchTerm = process.argv.shift().join(" ");
-
-
-// //Starts the application
-// liriBotActivate();
-// function liriBotActivate(){
-
-//   //User Input + Questions
-// inquirer.prompt([
-//   { type:"input",
-//     message:"Hello! I am Liri-Bot. What should I call you?",
-//     name:"user",
-// },
+//Variable that grabs the command argument
+var commandArg = process.argv.shift();
+var searchTerm = process.argv.join(" ");
 
 
-//   { type:"input",
-//     message:"Hello "+user+" Here's what I can help you with.\n [concert-this]\n[spotify-this-song]\n[movie-this]\n[do-what-it-says]",
-//     name:commandArg,searchTerm
-//   },    
-// ])
-// .then(function(commandArg,searchTerm) {
-// //Checks the conditions for which commandArg was typed
-//   switch(commandArg)
-//   {
-//     case 'concert-this':
-//     getConcert(searchTerm);
-//     break;
+//Starts the application
 
-//     case 'spotify-this-song':
-//     console.log("Ok searching for the song");
-//     spotifySong(searchTerm);
-//     break;
+function botCommands(commandArg, searchTerm) {
+  switch (commandArg) {
 
-//     case 'movie-this':
-//     console.log("Let's see what's near you shall we?");
-//     getMovieInfo(SearchTerm);
-//     break;
+    case 'concert-this':
+    if(commandArg === "")
+    {
+      console.log("No input detected");
+      break;
+    }
+      getConcert(searchTerm);
+      break;
 
-//     case 'do-what-it-says':
-//     console.log("Ok");
-//     doWhatItSays(SearchTerm);
-//     break;
-//   }
-// });
-// }
+    case 'spotify-this-song':
+    if(commandArg === "")
+    {
+      console.log("It seems you haven't typed anything any in so"+waitDot());
+      song = "The Sign";
+      spotifySong(song);
+      break;
+    }
+    else
+    {
+      console.log("Please hold searching...");
+      spotifySong(searchTerm);
+    }
+      break;
+
+    case 'movie-this':
+
+    if(commandArg === "")
+    {
+      var movie = "Mr. Nobody"
+      getMovieInfo(movie);
+      break;
+    }
+    else 
+    {
+      console.log("Great! Let's find your movie! One second...");
+      getMovieInfo(searchTerm);
+    }
+      break;
+
+    case 'do-what-it-says':
+    if(commandArg === "")
+    {
+      console.log("No Input Detected");
+      break;
+    }
+      console.log("Ok picking a random choice");
+      doWhatItSays();
+      break;
+  }
+
+}
 
 
+function getConcert(artists) {
+  request("https://rest.bandsintown.com/artists/" + artists + "/events?app_id=codingbootcamp", function(err,res,body)
+  {
+    if (err) {
+      console.log('Error:' + err);
+  }
+    let json = JSON.parse(body);
 
 
+    json.forEach(element => {
+      let concert = "Name: " + element.venue.name +
+      "\nCity: " + element.venue.city +
+      "\nState: " + element.venue.region +
+      "\nDate: " +  moment(element.datetime).format("MM/DD/YYYY");
+      console.log(concert);
+      console.log("-".repeat(30));
+     
+    });
+  });
+};
+
+function spotifySong(song) {
+  //Sends a request to the spotify API server using the song name
+  spotify.search({ type: 'track', query: song }, function (err, data) {
+    //displays error if errored
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    let songInfo = data.tracks.items[0];
+    let songDisplay = "Name: " + songInfo.artists[0].name+ 
+    "\nSong: " + songInfo.name +
+    "\nAlbum: " + songInfo.album.name
+    if(songInfo.preview_url != null){
+
+     +"\nPreview: " + songInfo.preview_url
+    }
+    else
+     {
+       console.log("Sorry no preview avaliable");
+     }
+    console.log(songDisplay); 
+
+  });
+};
 
 
-// function spotifySong(song){
-//   //Sends a request to the spotify API server using the song name
-//   spotify.search({type: 'track', query: song}, function (err, data) {
-//    //displays error if errored
-//     if (err) {
-//         return console.log('Error occurred: ' + err);
-//     }
-//       let songInfo = data.tracks.items[0];
-//       let songDisplay = console.log(songInfo.artist[0].name);
-//                         console.log(songInfo.name);
-//                         console.log(songInfo.album.name);
-//                         console.log(songInfo.preview_url);
-                        
-//     console.log(songDisplay);
+function getMovieInfo(movie) {
+  request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function(err,res,body)
+  {
+    if(err)
+    {
+      console.log("Error: " +err);
+    }
+
+    let json = JSON.parse(body);
+    let movieData = "Title:" + json.Title +
+      "\nYear: " + json.Year +
+      "\nIMDB: " + json.Ratings[0].Source +
+      "\nRotten Tomatoes: " + json.Ratings[1].Source +
+      "\nCountry: " + json.Country +
+      "\nLanguage: " + json.Language +
+      "\nPlot: " + json.Plot +
+      "\nActors: " +json.Actors;
+      console.log(movieData);
+      console.log("-".repeat(30));
     
-//   });
-//   };
+  });
+};
+
+function doWhatItSays() {
 
 
+  fs.readFile("./random.txt","utf8", function(err,data)
+  {
+    if(err)
+    {
+      console.log("Error: " + err);
+    }
+    let readData = data.split(",");
+    botCommands(readData[0],readData[1])
+  });
+};
+botCommands(commandArg, searchTerm);
 
-
-    
